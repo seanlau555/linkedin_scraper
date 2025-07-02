6b334e4eb3004e2a94add39a30ca18cb30f3a6cc
Sean Lau <lthon0225@gmail.com>
2025-07-02 15:45:19 +0800

    Update person.py

--- linkedin_scraper/person.py
+++ linkedin_scraper/person.py
@@ -562,7 +562,7 @@ class Person(Scraper):
         }
 
     def __repr__(self):
-        return "<Person {name}\n\nAbout\n{about}\n\nExperience\n{exp}\n\nEducation\n{edu}\n\nInterest\n{int}\n\nAccomplishments\n{acc}\n\nContacts\n{conn}>".format(
+        return "<Person {name}\n\nAbout\n{about}\n\nExperience\n{exp}\n\nEducation\n{edu}\n\nCertification\n{cert}\n\nInterest\n{int}\n\nAccomplishments\n{acc}\n\nContacts\n{conn}>".format(
             name=self.name,
             about=self.about,
             exp=self.experiences,
