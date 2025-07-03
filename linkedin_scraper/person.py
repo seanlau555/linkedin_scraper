1e7891d6e3fb80d06633dfed4d0a5a74532cb0c0
Sean Lau <lthon0225@gmail.com>
2025-07-03 15:44:31 +0800

    Update person.py

--- linkedin_scraper/person.py
+++ linkedin_scraper/person.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from dataclasses import asdict
 
 from linkedin_scraper import selectors
 
